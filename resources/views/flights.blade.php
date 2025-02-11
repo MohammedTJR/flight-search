f1f57802f3e57d785c8c536e2be16d5170518db8
MohammedTJR <med.touijar77@gmail.com>
2025-02-11 19:23:20 +0100

    11/02

--- resources/views/flights.blade.php
+++ resources/views/flights.blade.php
@@ -1,31 +1,40 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Resultados de Vuelos</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <style>
         .flight-header {
             display: flex;
             justify-content: space-between;
             align-items: center;
         }
+
         .flight-times {
             font-size: 1.2em;
             font-weight: bold;
         }
     </style>
 </head>
+
 <body class="bg-light">
     <div class="container mt-5">
         <h1 class="text-center">Resultados de Vuelos</h1>
         <a href="/" class="btn btn-secondary mb-4">Nueva búsqueda</a>
 
+        <div class="card shadow p-4 mt-4">
+            <h3 class="text-center">Evolución de Precios</h3>
+            <canvas id="priceChart"></canvas>
+        </div>
+
         @php
             $categorias = [
                 'Mejores vuelos' => $flights ?? [],
-                'Otros vuelos' => $other_flights ?? []
+                'Otros vuelos' => $other_flights ?? [],
             ];
         @endphp
 
@@ -34,7 +43,7 @@
                 <h2 class="mt-4">{{ $titulo }}</h2>
                 <div class="row">
                     @foreach ($listaVuelos as $flight)
-                        @if(isset($flight['flights'][0])) 
+                        @if (isset($flight['flights'][0]))
                             @php
                                 $detalleVuelo = $flight['flights'][0];
                                 $horaSalida = date('H:i', strtotime($detalleVuelo['departure_airport']['time']));
@@ -45,7 +54,8 @@
                                     <div class="card-body">
                                         <div class="flight-header">
                                             <h5 class="card-title">
-                                                <img src="{{ $detalleVuelo['airline_logo'] }}" alt="Logo" width="30">
+                                                <img src="{{ $detalleVuelo['airline_logo'] }}" alt="Logo"
+                                                    width="30">
                                                 {{ $detalleVuelo['airline'] }}
                                             </h5>
                                             <div class="flight-times">
@@ -54,12 +64,13 @@
                                         </div>
                                         <p class="card-text">
                                             <strong>Precio:</strong> €{{ $flight['price'] }} <br>
-                                            <strong>Duración total:</strong> 
-                                            {{ intdiv($flight['total_duration'], 60) }}h {{ $flight['total_duration'] % 60 }}min
+                                            <strong>Duración total:</strong>
+                                            {{ intdiv($flight['total_duration'], 60) }}h
+                                            {{ $flight['total_duration'] % 60 }}min
                                             <br>
-                                            <strong>Escalas:</strong> 
-                                            @if(!empty($flight['layovers']))
-                                                @foreach($flight['layovers'] as $layover)
+                                            <strong>Escalas:</strong>
+                                            @if (!empty($flight['layovers']))
+                                                @foreach ($flight['layovers'] as $layover)
                                                     {{ $layover['name'] }} ({{ $layover['duration'] }} min)
                                                 @endforeach
                                             @else
@@ -77,5 +88,56 @@
             @endif
         @endforeach
     </div>
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            fetch('/api/flights/prices') // Ajusta la URL según corresponda
+                .then(response => response.json())
+                .then(data => {
+                    const priceHistory = data.price_insights.price_history;
+
+                    // Convertir timestamps a fechas legibles
+                    const labels = priceHistory.map(entry => {
+                        const date = new Date(entry[0] * 1000);
+                        return date.toLocaleDateString();
+                    });
+
+                    const prices = priceHistory.map(entry => entry[1]);
+
+                    // Crear gráfico
+                    const ctx = document.getElementById("priceChart").getContext("2d");
+                    new Chart(ctx, {
+                        type: "line",
+                        data: {
+                            labels: labels,
+                            datasets: [{
+                                label: "Precio Histórico",
+                                data: prices,
+                                borderColor: "blue",
+                                backgroundColor: "rgba(0, 0, 255, 0.1)",
+                                fill: true
+                            }]
+                        },
+                        options: {
+                            responsive: true,
+                            scales: {
+                                x: {
+                                    title: {
+                                        display: true,
+                                        text: "Fecha"
+                                    }
+                                },
+                                y: {
+                                    title: {
+                                        display: true,
+                                        text: "Precio (€)"
+                                    }
+                                }
+                            }
+                        }
+                    });
+                })
+                .catch(error => console.error("Error cargando los datos:", error));
+        });
+    </script>
 </body>
 </html>
--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -2,189 +2,190 @@
 <html lang="es">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Buscar Vuelos</title>
-  <!-- Bootstrap CSS -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-  <!-- Select2 CSS -->
-  <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0/dist/css/select2.min.css" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Buscar Vuelos</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0/dist/css/select2.min.css" rel="stylesheet">
 </head>
 
 <body class="bg-light">
-  <div class="container mt-5">
-    <div class="text-center mb-4">
-      <img src="{{ asset('img/banner.jpg') }}" class="img-fluid" alt="FlyLow Banner">
-    </div>
-
-    <h1 class="text-center">Buscar Vuelos</h1>
-    <div class="card shadow p-4">
-    <form action="/flights" method="GET" id="flight-form">
-        <!-- Tipo de viaje -->
-        <div class="mb-3">
-            <label class="form-label">Tipo de viaje:</label>
-            <div class="btn-group w-100" role="group">
-                <input type="radio" class="btn-check" name="trip_type" id="ida_vuelta" value="1" checked>
-                <label class="btn btn-outline-primary w-50" for="ida_vuelta">Ida y vuelta</label>
-
-                <input type="radio" class="btn-check" name="trip_type" id="solo_ida" value="2">
-                <label class="btn btn-outline-primary w-50" for="solo_ida">Solo ida</label>
-            </div>
+    <div class="container mt-5">
+        <div class="text-center mb-4">
+            <img src="{{ asset('img/banner.jpg') }}" class="img-fluid" alt="FlyLow Banner">
         </div>
 
-        <!-- Organización en 2x2 -->
-        <div class="row">
-            <div class="col-md-6">
-                <label class="form-label">Origen:</label>
-                <select id="departure" name="departure" class="form-select" required>
-                    <option value="">Selecciona un aeropuerto...</option>
-                </select>
-            </div>
+        <h1 class="text-center">Buscar Vuelos</h1>
+        <div class="card shadow p-4">
+            <form action="/flights" method="GET" id="flight-form">
+                <div class="mb-3">
+                    <label class="form-label">Tipo de viaje:</label>
+                    <div class="btn-group w-100" role="group">
+                        <input type="radio" class="btn-check" name="trip_type" id="ida_vuelta" value="1" checked>
+                        <label class="btn btn-outline-primary w-50" for="ida_vuelta">Ida y vuelta</label>
 
-            <div class="col-md-6">
-                <label class="form-label">Destino:</label>
-                <select id="arrival" name="arrival" class="form-select" required>
-                    <option value="">Selecciona un aeropuerto...</option>
-                </select>
-            </div>
+                        <input type="radio" class="btn-check" name="trip_type" id="solo_ida" value="2">
+                        <label class="btn btn-outline-primary w-50" for="solo_ida">Solo ida</label>
+                    </div>
+                </div>
 
-            <div class="col-md-6">
-                <label class="form-label">Fecha de salida:</label>
-                <input type="date" name="date" class="form-control" required>
-            </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <label class="form-label">Origen:</label>
+                        <select id="departure" name="departure" class="form-select" required>
+                            <option value="">Selecciona un aeropuerto...</option>
+                        </select>
+                    </div>
 
-            <div class="col-md-6" id="return_date_contenedor">
-                <label class="form-label">Fecha de regreso:</label>
-                <input type="date" name="return_date" id="return_date" class="form-control">
-            </div>
+                    <div class="col-md-6">
+                        <label class="form-label">Destino:</label>
+                        <select id="arrival" name="arrival" class="form-select" required>
+                            <option value="">Selecciona un aeropuerto...</option>
+                        </select>
+                    </div>
 
-            <div class="col-md-6">
-                <label class="form-label">Clase:</label>
-                <select name="travel_class" class="form-select" required>
-                    <option value="">Selecciona una clase...</option>
-                    <option value="1">Económica</option>
-                    <option value="2">Económica Premium</option>
-                    <option value="3">Business</option>
-                    <option value="4">Primera clase</option>
-                </select>
-            </div>
+                    <div class="col-md-6">
+                        <label class="form-label">Fecha de salida:</label>
+                        <input type="date" name="date" class="form-control" required>
+                    </div>
 
-            <div class="col-md-6">
-                <label class="form-label">Pasajeros:</label>
-                <button type="button" class="btn btn-outline-secondary w-100" data-bs-toggle="dropdown" aria-expanded="false">
-                    Seleccionar Pasajeros
-                </button>
-                <div class="dropdown-menu w-100">
-                    <div class="d-flex p-3">
-                        <div class="me-3">
-                            <label>Adultos (12+ años)</label>
-                            <select id="adultos" name="adults" class="form-select">
-                                @for ($i = 1; $i <= 9; $i++)
-                                    <option value="{{ $i }}">{{ $i }} Adulto(s)</option>
-                                @endfor
-                            </select>
-                        </div>
-                        <div class="me-3">
-                            <label>Niños (2-11 años)</label>
-                            <select id="ninos" name="children" class="form-select">
-                                @for ($i = 0; $i <= 9; $i++)
-                                    <option value="{{ $i }}">{{ $i }} Niño(s)</option>
-                                @endfor
-                            </select>
-                        </div>
-                        <div class="me-3">
-                            <label>Bebés (con asiento, 0-1 año)</label>
-                            <select id="bebes_asiento" name="infants_in_seat" class="form-select">
-                                @for ($i = 0; $i <= 9; $i++)
-                                    <option value="{{ $i }}">{{ $i }} Bebé(s)</option>
-                                @endfor
-                            </select>
-                        </div>
-                        <div class="me-3">
-                            <label>Bebés (en regazo, 0-1 año)</label>
-                            <select id="bebes_regazo" name="infants_on_lap" class="form-select">
-                                @for ($i = 0; $i <= 9; $i++)
-                                    <option value="{{ $i }}">{{ $i }} Bebé(s)</option>
-                                @endfor
-                            </select>
+                    <div class="col-md-6" id="return_date_contenedor">
+                        <label class="form-label">Fecha de regreso:</label>
+                        <input type="date" name="return_date" id="return_date" class="form-control">
+                    </div>
+
+                    <div class="col-md-6">
+                        <label class="form-label">Clase:</label>
+                        <select name="travel_class" class="form-select" required>
+                            <option value="">Selecciona una clase...</option>
+                            <option value="1">Económica</option>
+                            <option value="2">Económica Premium</option>
+                            <option value="3">Business</option>
+                            <option value="4">Primera clase</option>
+                        </select>
+                    </div>
+
+                    <div class="col-md-6">
+                        <label class="form-label">Pasajeros:</label>
+                        <button type="button" class="btn btn-outline-secondary w-100" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            Seleccionar Pasajeros
+                        </button>
+                        <div class="dropdown-menu w-100">
+                            <div class="d-flex p-3">
+                                <div class="me-3">
+                                    <label>Adultos (12+ años)</label>
+                                    <select id="adultos" name="adults" class="form-select">
+                                        @for ($i = 1; $i <= 9; $i++)
+                                            <option value="{{ $i }}">{{ $i }} Adulto(s)</option>
+                                        @endfor
+                                    </select>
+                                </div>
+                                <div class="me-3">
+                                    <label>Niños (2-11 años)</label>
+                                    <select id="ninos" name="children" class="form-select">
+                                        @for ($i = 0; $i <= 9; $i++)
+                                            <option value="{{ $i }}">{{ $i }} Niño(s)</option>
+                                        @endfor
+                                    </select>
+                                </div>
+                                <div class="me-3">
+                                    <label>Bebés (con asiento, 0-1 año)</label>
+                                    <select id="bebes_asiento" name="infants_in_seat" class="form-select">
+                                        @for ($i = 0; $i <= 9; $i++)
+                                            <option value="{{ $i }}">{{ $i }} Bebé(s)</option>
+                                        @endfor
+                                    </select>
+                                </div>
+                                <div class="me-3">
+                                    <label>Bebés (en regazo, 0-1 año)</label>
+                                    <select id="bebes_regazo" name="infants_on_lap" class="form-select">
+                                        @for ($i = 0; $i <= 9; $i++)
+                                            <option value="{{ $i }}">{{ $i }} Bebé(s)</option>
+                                        @endfor
+                                    </select>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+
+                <div class="text-center mt-4">
+                    <button type="submit" class="btn btn-primary w-100">Buscar</button>
+                </div>
+            </form>
         </div>
 
-        <!-- Botón de búsqueda -->
-        <div class="text-center mt-4">
-            <button type="submit" class="btn btn-primary w-100">Buscar</button>
-        </div>
-    </form>
-</div>
+    </div>
 
-  </div>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0/dist/js/select2.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script>
+        jQuery.noConflict();
+        (function($) {
+            $(document).ready(function() {
+                $.getJSON('/airports', function(data) {
+                    var departureSelect = $('#departure');
+                    var arrivalSelect = $('#arrival');
 
-  <!-- Scripts al final del body para garantizar la carga correcta -->
-  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0/dist/js/select2.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-  <script>
-    // Evitar conflictos con otras librerías que puedan usar $
-    jQuery.noConflict();
-    (function($) {
-      $(document).ready(function() {
-        // Cargar aeropuertos desde Laravel
-        $.getJSON('/airports', function(data) {
-          var departureSelect = $('#departure');
-          var arrivalSelect = $('#arrival');
+                    Object.keys(data).forEach(function(ciudad) {
+                        var optgroup = $('<optgroup>', {
+                            label: ciudad
+                        });
+                        data[ciudad].forEach(function(aeropuerto) {
+                            var option = new Option(aeropuerto.name + ' (' + aeropuerto
+                                .iata + ')', aeropuerto.iata, false, false);
+                            optgroup.append(option);
+                        });
+                        departureSelect.append(optgroup.clone());
+                        arrivalSelect.append(optgroup.clone());
+                    });
 
-          Object.keys(data).forEach(function(ciudad) {
-            var optgroup = $('<optgroup>', { label: ciudad });
-            data[ciudad].forEach(function(aeropuerto) {
-              var option = new Option(aeropuerto.name + ' (' + aeropuerto.iata + ')', aeropuerto.iata, false, false);
-              optgroup.append(option);
+                    $('#departure, #arrival').select2({
+                        placeholder: "Escribe una ciudad o aeropuerto...",
+                        allowClear: true,
+                        width: '100%',
+                        minimumInputLength: 2,
+                        dropdownAutoWidth: true,
+                        templateResult: function(data) {
+                            return data.text;
+                        }
+                    });
+                });
+
+                let today = new Date().toISOString().split('T')[0];
+                $('input[name="date"]').attr('min', today);
+
+                $('input[name="date"]').change(function() {
+                    let departureDate = $(this).val();
+                    $('input[name="return_date"]').attr('min', departureDate);
+                });
+
+                $('input[name="trip_type"]').change(function() {
+                    if ($('#solo_ida').is(':checked')) {
+                        $('#return_date_contenedor').hide();
+                        $('#return_date').prop('disabled', true).val('');
+                    } else {
+                        $('#return_date_contenedor').show();
+                        $('#return_date').prop('disabled', false);
+                    }
+                });
+
+                $('#flight-form').submit(function(e) {
+                    var adultos = parseInt($('#adultos').val());
+                    var ninos = parseInt($('#ninos').val());
+                    var bebesAsiento = parseInt($('#bebes_asiento').val());
+                    var bebesRegazo = parseInt($('#bebes_regazo').val());
+                    var totalPasajeros = adultos + ninos + bebesAsiento + bebesRegazo;
+                    if (totalPasajeros > 9) {
+                        alert("El número total de pasajeros no puede superar los 9.");
+                        e.preventDefault();
+                    }
+                });
             });
-            departureSelect.append(optgroup.clone());
-            arrivalSelect.append(optgroup.clone());
-          });
-
-          // Inicializar select2 en los campos de aeropuerto
-          $('#departure, #arrival').select2({
-            placeholder: "Escribe una ciudad o aeropuerto...",
-            allowClear: true,
-            width: '100%',
-            minimumInputLength: 2,
-            dropdownAutoWidth: true,
-            templateResult: function(data) {
-              return data.text;
-            }
-          });
-        });
-
-        // Ocultar la fecha de regreso si se selecciona "Solo ida"
-        $('input[name="trip_type"]').change(function() {
-          if ($('#solo_ida').is(':checked')) {
-            $('#return_date_contenedor').hide();
-            $('#return_date').prop('disabled', true).val('');
-          } else {
-            $('#return_date_contenedor').show();
-            $('#return_date').prop('disabled', false);
-          }
-        });
-
-        // Validar que el total de pasajeros no supere 9
-        $('#flight-form').submit(function(e) {
-          var adultos = parseInt($('#adultos').val());
-          var ninos = parseInt($('#ninos').val());
-          var bebesAsiento = parseInt($('#bebes_asiento').val());
-          var bebesRegazo = parseInt($('#bebes_regazo').val());
-          var totalPasajeros = adultos + ninos + bebesAsiento + bebesRegazo;
-          if (totalPasajeros > 9) {
-            alert("El número total de pasajeros no puede superar los 9.");
-            e.preventDefault();
-          }
-        });
-      });
-    })(jQuery);
-  </script>
+        })(jQuery);
+    </script>
 </body>
 
 </html>

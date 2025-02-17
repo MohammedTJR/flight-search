c282b3c5d303a743f5f30f0259a652cfc97e9ad7
MohammedTJR <med.touijar77@gmail.com>
2025-02-17 20:11:07 +0100

    17/02

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Buscar Vuelos</title>
+    <title>FlyLow</title>
     <link rel="icon" type="image/png" href="{{ asset('img/favicon.png') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/search.css') }}">

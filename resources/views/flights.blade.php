f2f66f31cde09ac44b75c40e494bd8095841551f
MohammedTJR <med.touijar77@gmail.com>
2025-02-17 18:37:07 +0100

    subeme la radio

--- resources/views/flights.blade.php
+++ resources/views/flights.blade.php
@@ -7,6 +7,7 @@
     <title>Resultados de Vuelos</title>
     <link rel="icon" type="image/png" href="{{ asset('img/favicon.png') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <style>
         .flight-header {
@@ -125,7 +126,7 @@
             <h3 class="text-center">Información de Precios</h3>
             <p>
                 <strong>Nivel de precio:</strong> <span id="priceLevel"></span>
-                <span id="priceLevelBox" class="price-level-box"></span>
+                <i class="fa-solid fa-arrow-trend-up" style="color: #ff0000;"></i>
                 | <strong>Rango típico de precios:</strong> <span id="priceRange"></span> €
             </p>
         </div>
@@ -223,7 +224,7 @@
                 color = "red";
             }
 
-            document.getElementById("priceLevelBox").style.backgroundColor = color;
+            //document.getElementById("priceLevelBox").style.backgroundColor = color;
 
             document.getElementById("priceRange").innerText = priceRange.length === 2 ?
                 `${priceRange[0]} - ${priceRange[1]}` : "No disponible";

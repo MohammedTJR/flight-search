3af464ed117edd464f8d5df9bee7d1cd5d89af1c
MOTJR <med.touijar77@gmail.com>
2025-04-16 18:43:55 +0200

    Arreglado iconos

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -4,12 +4,6 @@
     <link rel="stylesheet" href="{{ asset('css/detalles.css') }}">
 @endsection
 
-@section('styles_ext')
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
-        integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-@endsection
-
 @section('script')
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 @endsection
--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -7,8 +7,10 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="manifest" href="/manifest.json">
     <link rel="icon" type="image/png" href="{{ asset('img/favicon.png') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+        integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     @yield('styles')
-    @yield('styles_ext')
     @yield('script')
     <title>@yield('titulo_pagina')</title>
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])

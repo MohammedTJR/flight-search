ac0bb48d69f3a83b8958709134bf6977e6ece687
MOTJR <med.touijar77@gmail.com>
2025-05-17 17:58:26 +0200

    Refactor logout routes in web.php to use Route::match for improved handling of GET and POST requests

--- routes/web.php
+++ routes/web.php
@@ -24,8 +24,7 @@ Route::get('/vuelo/{id}', [FlightController::class, 'show'])->name('flight.show'
 Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('login', [LoginController::class, 'login']);
 
-Route::get('logout', [LoginController::class, 'logout'])->name('logout');
-Route::post('logout', [LoginController::class, 'logout'])->name('logout');
+Route::match(['get', 'post'], 'logout', [LoginController::class, 'logout'])->name('logout');
 
 Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('register');
 Route::post('register', [RegisterController::class, 'register']);
